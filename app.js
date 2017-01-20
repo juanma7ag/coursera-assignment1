be24ec08a852e75705efbb6c0d9801f3acba8659
juanma7ag <juanma7ag@gmail.com>
2017-01-20 10:37:26 +0100

    Assignment 1, finished.

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 /**
  * Created by JuanMa7 on 20/1/17.
  */
+(function () {
+    'use strict';
+
+    angular.module('LCApp', []).controller('LCController', LCController);
+
+    LCController.$inject = ['$scope', '$filter'];
+    function LCController($scope, $filter) {
+        $scope.name = "JuanMa Ariza";
+        $scope.value = "";
+
+        $scope.commaCount = function () {
+            var v = $scope.value;
+            var numCommas = v.split(',');
+            if(numCommas.length > 3)
+                alert('Too Much!!!!!!!!!');
+            else
+                alert('Enjoy!!')
+        };
+    }
+})();
